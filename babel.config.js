69003e52a45f81e93041d16db04a5c3ae7255565
DiegoIpaez <diegoipaez2000@gmail.com>
2024-03-29 13:05:48 -0300

    init project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+const path = require('path');
+
+module.exports = function (api) {
   api.cache(true);
+
+  const moduleResolverPlugin = [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: [path.resolve('./')],
+        alias: {
+          '@': './',
+        },
+      },
+    ],
+  ];
+
   return {
+    plugins: [...moduleResolverPlugin],
     presets: ['babel-preset-expo'],
   };
 };
---
+++ components/map/Map.jsx
@@ -0,0 +1,6 @@
+import MapView from 'react-native-maps';
+import styles from './map.styles';
+
+export default function Map() {
+  return <MapView style={styles.container} />;
+}
