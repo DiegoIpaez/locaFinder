11074dabcab560f2d8f7af335bddb6fee24d5059
DiegoIpaez <diegoipaez2000@gmail.com>
2024-03-30 00:02:42 -0300

    The map and permissions for geolocation have been incorporated

--- components/map/Map.jsx
+++ components/map/Map.jsx
@@ -1,6 +1,57 @@
-import MapView from 'react-native-maps';
+import { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 import styles from './map.styles';
+import Loading from '../loading/Loading';
+import { ERROR_MSG, SUCCESSFUL_PERMISSION_STATUS } from '@/constants';
+import catchError from '@/utilities/catchError.utility';
+
+const INITIAL_REGION = {
+  latitude: -26.789837,
+  latitudeDelta: 0.04,
+  longitude: -65.220425,
+  longitudeDelta: 0.9,
+};
 
 export default function Map() {
-  return <MapView style={styles.container} />;
+  const [startCoords, setStartCoords] = useState(INITIAL_REGION);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getLocationPermission = async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== SUCCESSFUL_PERMISSION_STATUS) {
+          throw new Error(ERROR_MSG.PERMISSIONS.LOCATION.DENIED);
+        }
+        const { coords } = await Location.getCurrentPositionAsync();
+        setStartCoords((prev) => ({
+          ...prev,
+          latitude: coords?.latitude,
+          longitude: coords.longitude,
+          longitudeDelta: 120,
+        }));
+      } catch (error) {
+        catchError(error, ERROR_MSG.PERMISSIONS.LOCATION.DEFAULT);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getLocationPermission();
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <MapView style={styles.container} initialRegion={startCoords}>
+          <Marker coordinate={startCoords} />
+        </MapView>
+      )}
+    </>
+  );
 }
---
+++ constants/messages.constant.js
@@ -0,0 +1,9 @@
+export const ERROR_MSG = {
+  PERMISSIONS: {
+    LOCATION: {
+      DEFAULT: 'Hubo un error a la hora de solicitar los permisos.',
+      DENIED: 'Permiso denegado.',
+    },
+  },
+  GENERAL: 'Hubo un error, intentelo de nuevo mas tarde.'
+};
