797f68ca7f6560900e435a35ae236ef0d285c5c2
Diego Iván Paez <82846493+DiegoIpaez@users.noreply.github.com>
2024-03-31 23:35:01 -0300

    [FEATURE] loading component (#5)
    * a constant file has been generated for the colors
    
    * added activity indicator in Loading component

---
+++ constants/colors.constant.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  primary: '#0d6efd',
+  secondary: '#6c757d',
+  success: '#198754',
+  warning: '#ffc107',
+  danger: '#dc3545',
+  ligth: '#ffffff',
+  ligthGrey: '#efefef',
+  dark: 'black',
+};
